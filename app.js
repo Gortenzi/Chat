f0a0ef3e8f1d3157a18fdb3e7499d9811bf781cb
Tania <webtan4ik@gmail.com>
2022-02-18 18:46:47 +0200

    befor install parcel

--- app.js
+++ app.js
@@ -2,5 +2,29 @@ import {getCode, inputEmail, formInput } from './post.js'
 import {settingsModal, closeBtn, btns } from './modal.js'
 
 
+const urlPatch = 'https://chat1-341409.oa.r.appspot.com/api/user'
+const inputCode = document.querySelector('.input__code')
+const token = Cookies.set(inputCode)
 
-           
+ const setName = async () => {
+    try {
+        const response = await fetch(urlPatch, {
+         method: 'PATH',
+         headers: {
+           'Content-Type': 'application/json',
+           'Authorization': `Bearer ${token}`
+           },
+           body: ({
+           name:'TaniaGor'
+            })
+         });
+         const data = await response.json();
+     
+         console.log(data);
+       } catch(error) {
+           console.log(error)
+    } finally {
+        inputCode.form.reset()
+    }
+     }
+    // formInput.addEventListener('submit', installName )
