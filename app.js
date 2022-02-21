ac09548ae29031fb7f58f2150979f496d38f700d
Tania <webtan4ik@gmail.com>
2022-02-21 14:13:25 +0200

    add file request.js

--- app.js
+++ app.js
@@ -1,46 +1,59 @@
-import 'regenerator-runtime/runtime'
-import {getCode, inputEmail, formInput } from "./post.js"
+// import 'regenerator-runtime/runtime'
+// import {getCode, inputEmail, formInput } from "./post.js"
+import {UI_REQUEST, getRequest} from "./request.js"
 import {settingsModal, closeBtn, btns } from "./modal.js"
 import {sendmessagesBtn} from "./template.js"
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 
 
 
-const formForName = document.querySelector('.form__conf')
+// const formForName = document.querySelector('.form__conf')
 
   
-const setName = async () => {
-const urlPatch = 'https://chat1-341409.oa.r.appspot.com/api/user';
-const inputCode =  document.querySelector('.input__code')
-const userName = document.querySelector('.settings-input__name').value
-Cookies.set('magic-code', inputCode.value)
-const token = Cookies.get('magic-code')
-  console.log(userName)
-  console.log(token)
-   try {
-        const patchResponse = await fetch(urlPatch, {
-         method: 'PATCH',
-         body: JSON.stringify({'name':userName}),
-         headers: {
-           'Accept': 'aplication/json',
-           'Content-Type': 'application/json',
-           'Authorization': `Bearer ${token}`,
-           },
+// const setName = async () => {
+// const urlPatch = 'https://chat1-341409.oa.r.appspot.com/api/user';
+// const inputCode =  document.querySelector('.input__code')
+// const userName = document.querySelector('.settings-input__name').value
+// Cookies.set('magic-code', inputCode.value)
+// const token = Cookies.get('magic-code')
+//   console.log(userName)
+//   console.log(token)
+//    try {
+//         const patchResponse = await fetch(urlPatch, {
+//          method: 'PATCH',
+//          body: JSON.stringify({'name':userName.value}),
+//          headers: {
+//            'Accept': 'aplication/json',
+//            'Content-Type': 'application/json',
+//            'Authorization': `Bearer ${token}`,
+//            },
           
-          })
-         const data = await patchResponse.json();
-         console.log(JSON.stringify(data));
-     } catch(error) {
-           console.log(error)
-    } finally {
-      inputCode.form.reset()
+//           })
+//          const data = await patchResponse.json();
+//          console.log(JSON.stringify(data));
+//      } catch(error) {
+//            console.log(error)
+//     } finally {
+//       inputCode.form.reset()
      
-    }
-     }
-     formForName.addEventListener('submit', setName )
+//     }
+//      }
+//      formForName.addEventListener('submit', setName )
     
    
- 
+    //  Cookies.set('magic-code', inputCode.value)
+    //  const token = Cookies.get('magic-code')
      
+ 
+ 
+  UI_REQUEST.FORM_INPUT.addEventListener('submit', () =>{
+      const body = {
+        email: UI_REQUEST.INPUT_EMAIL.value
+      }
+  //  getRequest(METHOD.POST, UI_REQUEST.URL_POST, body)
+   getRequest('POST', UI_REQUEST.URL_POST, body)
+    })
 
-  
+
+    // UI_REQUEST_FORM_FOR_NAME.addEventListener('submit', setName )
+
---
+++ request.js
@@ -0,0 +1,40 @@
+export const UI_REQUEST = {
+    URL_PATCH: 'https://chat1-341409.oa.r.appspot.com/api/user',
+    URL_POST:'https://chat1-341409.oa.r.appspot.com/api/user',
+    URL_GET:'https://chat1-341409.oa.r.appspot.com/api/messages/',
+    FORM_FOR_NAME:document.querySelector('.form__conf'),
+    FORM_INPUT: document.querySelector('.form__autoconf'),
+    USER_NAME: document.querySelector('.settings-input__name'),
+    INPUT_CODE: document.querySelector('.input__code'),
+    INPUT_EMAIL: document.querySelector('.input__value'),
+    
+}
+
+
+export const getRequest = async (method, url, body = null, token = null) => {
+    const headers = {
+        'Accept': 'aplication/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+    }
+    try {
+        const response = await fetch (url, {
+            method: method,
+            headers: headers,
+            body: JSON.stringify(body)
+        })
+        const data = await response.json();
+        console.log(JSON.stringify(data));
+        return data
+    }catch(error) {
+        console.log(error)
+    }finally {
+        UI_REQUEST.INPUT_CODE.form.reset()
+        UI_REQUEST.INPUT_EMAIL.form.reset()
+
+    }
+
+}
+
+// UI_REQUEST_FORM_INPUT.addEventListener('submit', getCode )
+// UI_REQUEST_FORM_FOR_NAME.addEventListener('submit', setName )
